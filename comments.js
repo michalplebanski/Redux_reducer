7f61c505cba9d5b5bf97d51a8dafb20484b85c77
Michal <michalplebanski@gmail.com>
2018-05-27 16:44:38 +0200

    change object to array

--- comments.js
+++ comments.js
@@ -7,46 +7,41 @@ import { THUMB_DOWN } from "./actions";
 export const comments = (state = initialState, action) => {
 	switch (action.type) {
 		case ADD_COMMENT:
-			return Object.assign({}, state, {
-				comments: [
-					{
-						id: action.id,
-						text: action.text,
-						votesUp: 0,
-						votesDown: 0
-					},
-					...state
-				]
-			});
+			return [
+				{
+					id: action.id,
+					text: action.text,
+					votes: 0
+				},
+				...state
+			];
 		case REMOVE_COMMENT:
-			return Object.assign({}, state, {
-				comments: state.comments.filter(
-					comment => comment.id !== action.id
-				)
-			});
+			return state.filter(comment => comment.id !== action.id);
 		case EDIT_COMMENT:
-			return state.comments.map(comment => {
-				if (comment.id === action.id) {
-					comment.text = action.text;
-					return comment;
-				}
-				return comment;
-			});
-		case THUMB_UP_COMMENT:
 			return state.map(comment => {
 				if (comment.id === action.id) {
 					return {
-						comment,
+						...comment,
+						text: action.text
+					};
+				}
+				return comment;
+			});
+		case THUMB_UP:
+			return state.map(comment => {
+				if (comment.id === action.id) {
+					return {
+						...comment,
 						votes: comment.votes + 1
 					};
 				}
 				return comment;
 			});
-		case THUMB_DOWN_COMMENT:
+		case THUMB_DOWN:
 			return state.map(comment => {
 				if (comment.id === action.id) {
 					return {
-						comment,
+						...comment,
 						votes: comment.votes - 1
 					};
 				}
@@ -58,7 +53,4 @@ export const comments = (state = initialState, action) => {
 	}
 };
 
-const initialState = {
-	comments: [],
-	users: []
-};
+const initialState = [];
